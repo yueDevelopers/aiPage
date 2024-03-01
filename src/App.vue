83d07513c42f719da1f7e87ba87aed053b7be02c
孟子文 <mengziwen@tjmeiteng.com>
2024-03-01 15:01:52 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <textarea class="ask" placeholder="请输入问题" v-model="question" />
+    <button class="btn" @click="ask">提交</button>
+    <textarea class="ask" placeholder="回答" v-model="answer" />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+/* eslint-disable */
+const question = ref("");
+const answer = ref("");
+const ask = async () => {
+  const res = await axios.post("/api/meal/getMenu", {
+    ask: question.value,
+  });
+  answer.value = res.data.menu.text;
+};
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+  width: 100%;
+}
+.ask {
+  flex: 1;
+  margin: 10px;
+}
+.btn {
+  width: 100px;
+  align-self: center;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api/": {
+        target: "http://182.92.121.134:5000/",
+        changeOrigin: true,
+        pathRewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
